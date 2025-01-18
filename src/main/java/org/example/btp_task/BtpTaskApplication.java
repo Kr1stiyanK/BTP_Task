f1b5a0a8ccf9bacb7531457ee7d2d60e535e006e
Kr1stiyanK <kriskar1@abv.bg>
2025-01-19 01:50:50 +0200

    Initial repository commit

---
+++ src/main/java/org/example/btp_task/BtpTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.btp_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtpTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtpTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/btp_task/BtpTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.btp_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtpTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
